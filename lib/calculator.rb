0202732269d3b0864ab2b7f263d8b3c36a83ae2c
Jordan <jordansurgick27@gmail.com>
2021-09-27 03:47:32 -0400

    Update calculator.rb
    couldnt push to main so updated this way

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,77 +1,55 @@
-class Grocery
+puts "Please enter all the items purchased separated by a comma:"
+grocery_input = gets.chomp.strip
+#taking in the input here and stripping it.
 
-  @@grocery = {}
+items_price = {
+  milk: {unit_price: 3.97, sale_quantity: 2, sale_price: 5.00},
+  bread: {unit_price: 2.17, sale_quantity: 3, sale_price: 6.00},
+  banana: {unit_price: 0.99},
+  apple: {unit_price: 0.89},
+}
+#created a hash table with the data from the weekly priing table.
 
-  def initialize(item, quantity, price)
-    @item = item
-    @quantity = quantity
-    @price = price
+grocery_items = []
+#created an empty array to store the data.
+unless grocery_input.empty?
+  #loop for grocery input
+  grocery_list = grocery_input.split(',').collect(&:strip)
+  #made grocery list equal grocery input and stripped the comma and white space
+  grouped_grocery_list = grocery_list.group_by(&:itself)
+  #grouped the items
+  grouped_grocery_list.each { |k,v| grocery_items.push({name: k, quantity: v.length}) }
+  grocery_items.each do |item|
+    #parsed the data using a key value pair for name and quantity
+    item_name = item[:name].to_sym
+    #item name to a symbol
+    price = items_price[item_name]
+    if price[:sale_quantity]
+      sales = item[:quantity] / price[:sale_quantity]
+      #divided the item quantity by the sale quantity
+      units = item[:quantity] % price[:sale_quantity]
+      #gives us the remainder from using the module operator
+      item[:price] = (units * price[:unit_price]) + (sales * price[:sale_price])
+      #multiplying the units times price plus sales price to come up with the item price
+      supposed_price = item[:quantity] * price[:unit_price]
+      #price
+      item[:saved_money] = supposed_price - item[:price]
+      #subtracting to get the money saved
+    else
+      item[:price] = item[:quantity] * price[:unit_price]
+      item[:saved_money] = 0
+      #different logic if the first isnt true, will show that no money was saved
+    end
+    item
+    #returning the item
   end
-
-  def self.all
-    @@grocery
-  end
 end
 
-class Sale_item
-  @@sale_items = {}
-
-  attr_reader = :item, :unit_price, :sale_price
-
-  def initialize(item, unit_price, sale_price)
-    @item = item
-    @unit_price = unit_price
-    @sale_price = sale price
-  end
-
-  def initialize(item, unit_price, sale_price)
-    @@sale_items[item] = new(item: item, unit_price: unit_price, sale_price: sale_price)
-  end
-
-  def self.all
-    @@sale_item
-  end
-end
-
-def items_bought
-  def list
-  puts "Please enter all the items purchased separated by a comma:"
-  @grocery = gets.chomp
-  end
-end
-
-private
-
-def total_bill
-
-end
-
-def show_price_table
-  puts "Item     Unit price        Sale price"
-  puts "--------------------------------------"
-  puts "Milk      $3.97            2 for $5.00"
-  puts "Bread     $2.17            3 for $6.00"
-  puts "Banana    $0.99"
-  puts "Apple     $0.89"
-end
-
-def list_items
-  puts "Item     Quantity      Price"
-  puts "--------------------------------------"
-  puts "#{item}      #{quantity}            #{price}"
-  puts "Total price : #{total}"
-  puts "You saved #{saved_money} today."
-  
-end
-
-begin
-  Grocery.new('banana', 1,  0.99)
-  Grocery.new('apple', 1,  0.89)
-  Grocery.new('milk', 1, 3.97)
-  Grocery.new('bread', 1, 2.17)
-  #Sale_item.new('milk', 2, 5.00)
-  #Sale_item.new('bread', 3, 6.00)
-
-  grocery = Grocery.new
-  #grocery.sum_bill
-end
+#printed the table
+puts "Item     Quantity      Price"
+puts "--------------------------------------"
+grocery_items.each { |item| puts "#{item[:name]}      #{item[:quantity]}            #{item[:price]}" }
+#parsed with the each method to list the varibles needed
+puts "Total price : #{sprintf("%.2f", grocery_items.sum { |h| h[:price] })}"
+#used sprintf "%.2f" to turn the float number into a rounded number. 2 decimal points
+puts "You saved #{sprintf("%.2f", grocery_items.sum { |h| h[:saved_money] })} today."
